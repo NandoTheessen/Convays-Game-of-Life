d3474d1fcf2b637857dc343e64d96dc7744e2ac9
NandoTheessen <nando.theessen@gmail.com>
2018-12-11 00:58:06 +0100

    Display grid on canvas, bring cells to life on click
    - add useRef hook to set up canvas ref
    - Implement logic to paint grid
    - Implement logic of clicking on cells

--- src/src/App.js
+++ src/src/App.js
@@ -3,25 +3,51 @@ import React, { useState, useEffect, useRef } from 'react';
 const App = props => {
   const [height, setHeight] = useState(300);
   const [width, setWidth] = useState(300);
-
+  const [shouldanimate, setShouldanimate] = useState(false);
+  const [timestamp, setTimestamp] = useState(Date.now());
   const canvasRef = useCanvas();
+
+  const animate = timestamp => {
+    if (shouldanimate) {
+      requestAnimationFrame(timestamp => animate(timestamp));
+    }
+  };
+  const determinePosition = e => {
+    const pos = document
+      .getElementsByTagName('canvas')[0]
+      .getBoundingClientRect();
+    console.log(e.clientX - pos.x, e.clientY - pos.y);
+    const xAxis = Math.floor((e.clientX - pos.x) / 15);
+    const yAxis = Math.floor((e.clientY - pos.y) / 15);
+    const ctx = canvasRef.current.getContext('2d');
+
+    ctx.fillRect(
+      e.clientX - pos.x - ((e.clientX - pos.x) % 15),
+      e.clientY - pos.y - ((e.clientY - pos.y) % 15),
+      15,
+      15
+    );
+    console.log(xAxis, yAxis);
+  };
+
   console.log(canvasRef);
   return (
     <canvas
       ref={canvasRef}
-      style={{ border: '1px solid black' }}
+      style={{ border: '1px solid black', marginTop: '200px' }}
       height={height}
       width={width}
+      onClick={determinePosition}
     />
   );
   function useCanvas(context = '2d') {
     const canvasRef = useRef(null);
-    const [timestamp, setTimestamp] = useState(Date.now());
-    const [shouldanimate, setShouldanimate] = useState(false);
 
-    useEffect(timestamp => {
+    useEffect(() => {
       const ctx = canvasRef.current.getContext(context);
       ctx.beginPath();
+      ctx.lineWidth = 0.1;
+      ctx.strokeStyle = 'rgba(0, 0, 0, 0.2)';
       for (let i = 15, l = 300; i < l; i += 15) {
         ctx.moveTo(i, 0);
         ctx.lineTo(i, 300);
@@ -32,7 +58,7 @@ const App = props => {
           ctx.stroke();
         }
       }
-      let animationFrameId = requestAnimationFrame(timestamp => {
+      let animationFrameId = requestAnimationFrame(() => {
         animate(timestamp);
       });
 
@@ -40,11 +66,6 @@ const App = props => {
     }, []);
 
     return canvasRef;
-    function animate(timestamp) {
-      if (shouldanimate) {
-        requestAnimationFrame(timestamp => animate(timestamp));
-      }
-    }
   }
 };
 
