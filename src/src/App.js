723afa15d2355e71a3efb521ba732251e0bcbf6c
NandoTheessen <nando.theessen@gmail.com>
2018-12-11 19:49:19 +0100

    Implement drawing logic, refactor useEffect hook, refactor drawing logic
    - Implement drawing logic for grid
    - Refactor drawing logic to be more efficient
    - removed useEffect hook from seperate function and implemented it in main component

--- src/src/App.js
+++ src/src/App.js
@@ -1,70 +1,79 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 const App = props => {
-  const [height, setHeight] = useState(300);
-  const [width, setWidth] = useState(300);
+  const width = 300;
+  const height = 300;
+  const [iterator, setIterator] = useState(15);
   const [shouldanimate, setShouldanimate] = useState(false);
   const [timestamp, setTimestamp] = useState(Date.now());
-  const canvas = useCanvas();
+  const [life, setLife] = useState([]);
+  const canvas = useRef(null);
 
   const animate = timestamp => {
+    const ctx = canvas.current.getContext('2d');
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = '#696969';
+
+    for (let i = iterator, l = height; i < l + iterator; i += iterator) {
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, height);
+      for (let j = iterator, l = width; j < l + iterator; j += iterator) {
+        ctx.moveTo(0, j);
+        ctx.lineTo(i, j);
+      }
+    }
+    ctx.stroke();
     if (shouldanimate) {
+      console.log('animating!');
       requestAnimationFrame(timestamp => animate(timestamp));
     }
   };
-  const determinePosition = e => {
-    const pos = document
-      .getElementsByTagName('canvas')[0]
-      .getBoundingClientRect();
-    console.log(e.clientX - pos.x, e.clientY - pos.y);
-    const xAxis = Math.floor((e.clientX - pos.x) / 15);
-    const yAxis = Math.floor((e.clientY - pos.y) / 15);
-    const ctx = canvas.current.getContext('2d');
 
-    ctx.fillRect(
-      e.clientX - pos.x - ((e.clientX - pos.x) % 15),
-      e.clientY - pos.y - ((e.clientY - pos.y) % 15),
-      15,
-      15
-    );
-    console.log(xAxis, yAxis);
-  };
-
-  console.log(canvas);
-  return (
-    <canvas
-      ref={canvas}
-      style={{ border: '1px solid black', marginTop: '200px' }}
-      height={height}
-      width={width}
-      onClick={determinePosition}
-    />
-  );
-  function useCanvas(context = '2d') {
-    const canvas = useRef(null);
-
-    useEffect(() => {
-      const ctx = canvas.current.getContext(context);
-      ctx.lineWidth = 0.1;
-      ctx.strokeStyle = 'rgba(0, 0, 0, 0.2)';
-      for (let i = 15, l = 500; i < l; i += 15) {
-        for (let j = 15, l = 500; j < l; j += 15) {
-          ctx.moveTo(i, 0);
-          ctx.lineTo(i, j);
-          ctx.moveTo(0, j);
-          ctx.lineTo(i, j);
-          ctx.stroke();
-        }
-      }
+  useEffect(
+    timestamp => {
+      setTimestamp(Date.now() - timestamp);
       let animationFrameId = requestAnimationFrame(() => {
         animate(timestamp);
       });
 
       return () => cancelAnimationFrame(animationFrameId);
-    }, []);
+    },
+    [life]
+  );
+  return (
+    <>
+      <canvas
+        ref={canvas}
+        style={{ border: '1px solid black', marginTop: '200px' }}
+        height={height}
+        width={width}
+        onClick={e => determinePosition(e, canvas, iterator)}
+      />
+      <button onClick={shouldanimate => setShouldanimate(!shouldanimate)}>
+        Play!
+      </button>
+      <button onClick={() => setLife([1, 2, 3, 4, 5, 6])}>
+        Trigger useEffect!
+      </button>
+    </>
+  );
+};
 
-    return canvas;
-  }
+const determinePosition = (e, canvas, iterator) => {
+  const pos = document
+    .getElementsByTagName('canvas')[0]
+    .getBoundingClientRect();
+  const xAxis = Math.floor((e.clientX - pos.x) / iterator);
+  const yAxis = Math.floor((e.clientY - pos.y) / iterator);
+  const ctx = canvas.current.getContext('2d');
+
+  ctx.fillRect(
+    e.clientX - pos.x - ((e.clientX - pos.x) % iterator),
+    e.clientY - pos.y - ((e.clientY - pos.y) % iterator),
+    iterator - 1,
+    iterator - 1
+  );
+  console.log(xAxis, yAxis);
 };
 
 export default App;
