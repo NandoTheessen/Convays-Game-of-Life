378b32ab09efe50ab8c7038a0436959553c40fd9
NandoTheessen <nando.theessen@gmail.com>
2018-12-13 00:07:50 +0100

    Change to the way the game checks for neighbours of the current cell

--- src/src/util/GameOfLife.js
+++ src/src/util/GameOfLife.js
@@ -12,22 +12,13 @@ class Life {
     for (let x = 0, l = currentGen.length; x < l; x++) {
       for (let y = 0, l = currentGen[x].length; y < l; y++) {
         let sum = 0;
-        if (x !== 19 && y !== 19 && y !== 0 && x !== 0) {
-          //   console.log('X: ', x, 'Y: ', y);
-          for (let r = -1, l = 1; r <= l; r++) {
-            for (let c = -1, l = 1; c <= l; c++) {
-              if (newGen[x + r][y + c].alive) sum += 1;
-              //   console.log('X :', x + r, 'Y :', y + r, 'sum: ', sum);
-            }
-          }
-          if (newGen[x][y].alive) sum--;
-          //   console.log('X :', x, 'Y :', y, 'sum; ', sum);
-        } else if (x === 19) {
-          //   console.log('X===19');
-        } else if (y === 19) {
-          //   console.log('Y===19');
-        }
 
+        //   console.log('X: ', x, 'Y: ', y);
+        for (let r = -1, l = 1; r <= l; r++) {
+          for (let c = -1, l = 1; c <= l; c++) {
+            //   console.log('X :', x + r, 'Y :', y + r, 'sum: ', sum);
+          }
+        }
         if ((sum > 3 || sum < 2) && newGen[x][y].alive) {
           newGen[x][y].toggleState();
         } else if (sum === 3 && !newGen[x][y].alive) {
